ab06fdfc30ec4f6e067f34acc0b738d1a6f8d5d6
malek karzoun <karzounmalek99@gmail.com>
2022-11-26 20:57:36 +0100

    Implemented task 1

---
+++ javascript/javascript1/week1/task1+2.js
@@ -0,0 +1,24 @@
+// A future age calculator
+
+let yearOfBirth = 1987;
+let yearOfFuture = 2027;
+const age = yearOfFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearOfFuture}`)
+
+
+// A dog age calculator
+
+let dogYearOfBirth = 2017 ;
+let dogYearOfFuture = 2027 ;
+let dogYear= dogYearOfFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = false;
+
+if (shouldShowResultInDogYears) {
+  console.log(`Your dog will be ${dogYear} human years old in 2027`)
+} else {
+  console.log(`Your dog will be ${dogYear*7} dog years old in 2027`)
+}
+
+
+
+
